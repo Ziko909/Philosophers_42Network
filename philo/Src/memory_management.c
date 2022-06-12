d997ae33fb45ea313240c4187512722bf22e1058
Ziko909 <zikoziko078@gmail.com>
2022-06-12 17:08:23 +0100

    The Mandatory Part has been completed

---
+++ philo/Src/memory_management.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   memory_management.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zaabou <zaabou@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/11 20:27:28 by zaabou            #+#    #+#             */
+/*   Updated: 2022/06/12 16:56:24 by zaabou           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/philo.h"
+
+t_ph_in	*initialize_data(t_ph_in *head)
+{
+	head = malloc(sizeof(t_ph_in));
+	if (!head)
+		return (NULL);
+	head->ptr_s = malloc(sizeof(t_philo));
+	if (!head->ptr_s)
+		return (NULL);
+	head->ptr_s->n_of_alloc = 1;
+	head->ptr_s->n_e = 0;
+	head->ptr_s->simulation_state = 1;
+	return (head);
+}
+
+int	ft_cleaning_of_memmory(t_ph_in *head)
+{
+	int		n_of_alloc;
+	t_ph_in	*tmp;
+
+	n_of_alloc = head->ptr_s->n_of_alloc;
+	free(head->ptr_s);
+	while (--n_of_alloc)
+	{
+		tmp = head;
+		if (pthread_mutex_destroy(&tmp->forks))
+			return (0);
+		head = head->next;
+		free (tmp);
+	}
+	return (1);
+}
